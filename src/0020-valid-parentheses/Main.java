dfd0041d1510063b787f9d14422fa7e2f1739d5b
aszed <ywm00@qq.com>
2019-02-16 18:15:27 +0800

    day-02-16

---
+++ src/0020-valid-parentheses/Main.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+/**
+ * @author yuanweimin(aszed)
+ * @title linked-list-cycle
+ * @id #0141
+ */
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> map = new HashMap<>();
+        map.put('[', ']');
+        map.put('{', '}');
+        map.put('(', ')');
+        for (Character ele : s.toCharArray()) {
+            if (stack.size() == 0) {
+                stack.push(ele);
+            } else if (map.get(stack.peek()) == ele) {
+                stack.pop();
+            } else {
+                stack.push(ele);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String str = "()[]{}";
+        System.out.println(solution.isValid(str));
+    }
+}
