678a35db83620fabfcc39699ac4d889d92f23d73
aszed <ywm00@qq.com>
2019-02-18 13:34:36 +0800

    day-02-18

--- src/0239-sliding-window-maximum/Main.java
+++ src/0239-sliding-window-maximum/Main.java
@@ -1,4 +1,8 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 /**
  * @author yuanweimin(aszed)
@@ -7,8 +11,38 @@ import java.util.Arrays;
  */
 
 class Solution {
+    PriorityQueue<Integer> window;
+    Queue<Integer> queue = new LinkedList<>();
+    ArrayList<Integer> res = new ArrayList<>();
+
     public int[] maxSlidingWindow(int[] nums, int k) {
-        return new int[0];
+        if (nums.length == 0 || k == 0) return new int[0];
+
+        window = new PriorityQueue<>(k, (x, y) -> y - x);
+        for (int ele : nums) {
+            slide(ele, k);
+        }
+
+        int[] arr = new int[res.size()];
+        for (int i=0;i<res.size();i++) {
+            arr[i] = res.get(i);
+        }
+
+        return arr;
+    }
+
+    public void slide(int val, int k) {
+        if (queue.size() < k) {
+            queue.offer(val);
+        } else {
+            queue.offer(val);
+            queue.poll();
+        }
+        if (queue.size() == k) {
+            window.addAll(queue);
+            res.add(window.peek());
+            window.clear();
+        }
     }
 }
 
