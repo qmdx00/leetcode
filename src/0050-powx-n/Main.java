62abb5e417124b42bb14a4d7731ac6277ed7985b
aszedd <ywm00@qq.com>
2019-02-23 20:41:47 +0800

    day-02-23

--- src/0050-powx-n/Main.java
+++ src/0050-powx-n/Main.java
@@ -4,18 +4,29 @@
  * @id #0050
  */
 
+/* timeout
+class Solution {
+    public double myPow(double x, int n) {
+        if (n == 0)
+            return 1;
+        if (n < 0)
+            return 1 / myPow(x, -n);
+        if (n % 2 != 0)
+            return x * myPow(x, n - 1);
+        return myPow(x * x, n / 2);
+    }
+}
+*/
+
 class Solution {
     public double myPow(double x, int n) {
         double res = 1.0;
-        if (n > 0) {
-            for (int i = 0; i < n; i++)
+        for (int i = n; i != 0; i /= 2) {
+            if (i % 2 != 0)
                 res *= x;
-        } else if (n < 0) {
-            for (int i = 0; i < -n; i++) {
-                res *= 1.0 / x;
-            }
+            x *= x;
         }
-        return res;
+        return n < 0 ? 1 / res : res;
     }
 }
 
