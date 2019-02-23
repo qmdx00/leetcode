7992bf5985b8a918b39813b46a682863caedf40b
aszedd <ywm00@qq.com>
2019-02-23 19:49:41 +0800

    day-02-23

---
+++ src/0050-powx-n/Main.java
@@ -0,0 +1,20 @@
+/**
+ * @author yuanweimin(aszed)
+ * @title powx-n
+ * @id #0050
+ */
+
+class Solution {
+    public double myPow(double x, int n) {
+        return 0.0;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        double x = 2.00;
+        int n = 10;
+        System.out.println(solution.myPow(x, n));
+    }
+}
